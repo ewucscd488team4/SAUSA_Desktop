426b01b18e2b6e47572abc59af07a8bc1ea1d0ac
rhoughton13 <50637246+rhoughton13@users.noreply.github.com>
2020-01-06 12:28:48 -0800

    Fixed minor typos in menus and implemented app side communication
    Corrected a few typos in the menus from the most recent update, ensuring no cascading issues from changing text, and implemented a basic output communication skeleton for sending data to embedded unity

---
+++ SAUSALibrary/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using SAUSALibrary.Defaults;
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using WPFUI.ViewModels;
+
+namespace WPFUI.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        static extern bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+
+        internal delegate int WindowEnumProc(IntPtr hwnd, IntPtr lparam);
+        [DllImport("user32.dll")] 
+        internal static extern bool EnumChildWindows(IntPtr hwnd, WindowEnumProc func, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int msg, int wParam, IntPtr lParam);
+        
+        private Process process;
+        private IntPtr unityHWND = IntPtr.Zero;
+
+        private const int WM_ACTIVATE = 0x0006;
+        private const int WA_ACTIVE = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializePanel();
+            DataContext = new MainWindowViewModel();
+            try
+            {
+                process = new Process();
+                process.StartInfo.FileName = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "\\EmbedTest\\Child.exe";
+                process.StartInfo.Arguments = "-parentHWND " + unityPanel.Handle.ToInt32() + " " + Environment.CommandLine;
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                process.WaitForInputIdle();
+
+                EnumChildWindows(unityPanel.Handle, WindowEnum, IntPtr.Zero);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ".\nCannot find Unity file: " + process.StartInfo.FileName);
+            }
+            try
+            {
+                //Testing communication between Unity and Application
+                File.WriteAllText(System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "\\EmbedTest\\TestFile.txt", "[10, 10, 20]");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ".\nUnable to create communication file.");
+            }
+        }
+
+        private void InitializePanel()
+        {
+            this.unityPanel.TabIndex = 1;
+            this.unityPanel.TabStop = true;
+            this.unityPanel.Resize += new EventHandler(this.UnityPanelResize);
+        }
+
+        private void FileNewDesign_Click(object sender, RoutedEventArgs e)
+        {
+            NewBuild newBuild = new NewBuild();
+            newBuild.Show();
+        }
+
+        private void FileOpenDesign_Click(object sender, RoutedEventArgs e)
+        {
+            OpenBuild openBuild = new OpenBuild();
+            openBuild.Show();
+        }        
+
+        private void FilePref_Click(object sender, RoutedEventArgs e)
+        {
+            Preferences prefs = new Preferences();
+            prefs.Show();
+        }
+
+        private void EditStorageAttribs_Click(object sender, RoutedEventArgs e)
+        {
+            StorageArea storageArea = new StorageArea();
+            storageArea.Show();
+        }
+
+        private void EditDatabaseParam_Click(object sender, RoutedEventArgs e)
+        {
+            DatabaseParameters dataPrefs = new DatabaseParameters();
+            dataPrefs.Show();
+        }
+
+        private void EditCrateAttribs_Click(object sender, RoutedEventArgs e)
+        {
+            CrateAttributes cratePrefs = new CrateAttributes();
+            cratePrefs.Show();
+        }
+
+        /// <summary>
+        /// Clears the scratch folder and closes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(FilePathDefaults.ScratchFolder);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            this.Close();
+        }
+
+        private void HelpHelp_Click(object sender, RoutedEventArgs e)
+        {
+            Help help = new Help();
+            help.Show();
+        }
+
+        private void HelpAbout_Click(object sender, RoutedEventArgs e)
+        {
+            About about = new About();
+            about.Show();
+        }
+
+        private void HelpUpdates_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateNotImplemented update = new UpdateNotImplemented();
+            update.Show();
+        }
+
+        private int WindowEnum(IntPtr hwnd, IntPtr lparam)
+        {
+            unityHWND = hwnd;
+            ActivateUnityWindow();
+            return 0;
+        }
+        private void ActivateUnityWindow()
+        {
+            SendMessage(unityHWND, WM_ACTIVATE, WA_ACTIVE, IntPtr.Zero);
+        }
+        private void UnityPanelResize(object sender, EventArgs e)
+        {
+            MoveWindow(unityHWND, 0, 0, unityPanel.Width, unityPanel.Height, true);
+            ActivateUnityWindow();
+        }
+    }
+}
