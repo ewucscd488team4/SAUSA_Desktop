3d4f744ed3a735ade876ddfe5b43ea8113baa902
Mark Dietzler <mdietzler@eagles.ewu.edu>
2020-02-05 17:25:08 -0800

    GUI Update 22 add 2 error views
    Add DirectoryNotFound vie and NewRoomError dialog

---
+++ WPFUI/Views/ErrorViews/DirectoryNotFoundError.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WPFUI.Views.ErrorViews
+{
+    /// <summary>
+    /// Interaction logic for DirectoryNotFoundError.xaml
+    /// </summary>
+    public partial class DirectoryNotFoundError : Window
+    {
+        public DirectoryNotFoundError()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            //closes the Window, all other processing happens in the view model
+            this.Close();
+        }
+    }
+}
---
+++ WPFUI/Views/ErrorViews/NewRoomErrorDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WPFUI.Views.ErrorViews
+{
+    /// <summary>
+    /// Interaction logic for NewRoomErrorDialog.xaml
+    /// </summary>
+    public partial class NewRoomErrorDialog : Window
+    {
+        public NewRoomErrorDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            //closes the Window, all other processing happens in the view model
+            this.Close();
+        }
+    }
+}
