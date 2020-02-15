0eacd94fb77a0e48cca06c13149c376c15f840b2
Mark Dietzler <mdietzler@eagles.ewu.edu>
2020-02-14 19:35:15 -0800

    GUI Update 26 external DB paremeters
    Work continues on the external DB parameters view in the edit portion of the menu. Testing works against hard coded db parameters in the code (too difficult to keep typing them in), next up is getting export to work. I'm using my own sparterASP account to test against, the db values are in the editviewmodel code.

---
+++ SAUSALibrary/FileHandling/Database/Writing/WriteExternalDB.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SAUSALibrary.FileHandling.Database.Writing
+{
+    public class WriteExternalDB
+    {
+        public static bool TestMySQL_DBConnection(string[] testParameters)
+        {
+            MySqlConnection conn;
+            MySqlConnectionStringBuilder dbConString = new MySqlConnectionStringBuilder
+            {
+                Server = testParameters[0],
+                Database = testParameters[1],
+                UserID = testParameters[2],
+                Password = testParameters[3]
+            };
+            bool returnMe = false;
+            try
+            {
+                using (conn = new MySqlConnection(dbConString.ConnectionString))
+                {
+                    conn.Open();
+                    returnMe = true;
+                }
+
+            }
+            catch (MySqlException)
+            {
+                return returnMe;
+            }
+            return returnMe;
+        }
+
+        public static bool TestMSSQL_DBConnection(string[] testParameters)
+        {
+            return false;
+        }
+
+        public static bool SetUpMySQLDatabase(string[] dbparemeters)
+        {
+            /*
+             CREATE TABLE TestData (
+            crateIndex smallint(4) NOT NULL AUTO_INCREMENT,
+            xPos Decimal(10,4),
+            yPos Decimal(10,4),
+            zPos Decimal(10,4),
+            length Decimal(8,4) NOT NULL,
+            width Decimal(8,4) NOT NULL,
+            height Decimal(8,4) NOT NULL,
+            weight Decimal(7,2) NOT NULL,
+            name VARCHAR(150) NOT NULL,
+            PRIMARY KEY (crateIndex)
+             */
+            return false;
+        }
+    }
+}
--- WPFUI/ViewModels/EditViewModel.cs
+++ WPFUI/ViewModels/EditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using SAUSALibrary.FileHandling.Database.Writing;
 using System;
 using System.Windows;
 
@@ -7,21 +8,64 @@ namespace WPFUI.ViewModels
 {
     public class EditViewModel : ViewModelBase
     {
+        #region test data
+
+        private const string _SmarterASPTestServer = "MYSQL5022.site4now.net";
+
+        private const string _SmarterASPTestDB = "db_a4733c_sausa";
+
+        private const string _SmarterASPTestUserID = "a4733c_sausa";
+
+        private const string _SmarterASPTestPassword = "Sausa#1test";
+
+        private string[] _HardCodedTestDBFields = new string[] { _SmarterASPTestServer, _SmarterASPTestDB, _SmarterASPTestUserID, _SmarterASPTestPassword };
+
+        #endregion
+
         #region fields
 
-        public bool[] DataBaseArray { get; } = new bool[] { true, false, false };
+        private string? _TestSuccessOrFailString;
+
+        public string? TestSuccessOrFailString
+        {
+            get => _TestSuccessOrFailString;
+            set => Set(ref _TestSuccessOrFailString, value);
+        }
+
+        public bool[]? DataBaseArray { get; } = new bool[] { true, false, false };
+
+        private string[]? _DBFieldData;
+
+        public string[]? DBFieldData
+        {
+            get => _DBFieldData;
+            set => Set(ref _DBFieldData, value);
+        }
+
+        private Visibility _TestFieldVisibility;
+
+        public Visibility TestFieldVisibility
+        {
+            get => _TestFieldVisibility;
+            set => Set(ref _TestFieldVisibility, value);
+        }
 
         #endregion
 
         #region commands
 
-        public RelayCommand<Window>? ApplySettings { get; private set; }
+        public RelayCommand<Window>? ApplyDatabaseSettings { get; private set; }
+        public RelayCommand? TestDatabaseSettings { get; private set; }
 
         #endregion
 
         public EditViewModel()
         {
-            ApplySettings = new RelayCommand<Window>(OnApplySettings);
+            TestFieldVisibility = Visibility.Hidden;
+            ApplyDatabaseSettings = new RelayCommand<Window>(OnApplySettings);
+            TestDatabaseSettings = new RelayCommand(OnTestDataBaseSettings);
+            DBFieldData = new string[] { "WW", "XX", "YY", "ZZ" };
+            TestSuccessOrFailString = "Press Test to test your entries";
         }
 
         #region command methods
@@ -34,6 +78,36 @@ namespace WPFUI.ViewModels
             {
                 window.Close();
             }
+        }
+
+        /// <summary>
+        /// Tests the given mysql connection paremeters and reports success if they work, fail if they do not.
+        /// </summary>
+        private void OnTestDataBaseSettings()
+        {
+            if (!CheckDBFields())
+            {
+                //if (WriteExternalDB.TestMySQL_DBConnection(DBFieldData)) //use field data from view
+                if (WriteExternalDB.TestMySQL_DBConnection(_HardCodedTestDBFields)) //hard coded to work with smarterASP test database; for testing
+                {
+                    TestSuccessOrFailString = "TEST SUCCESSFUL";
+                }
+                else
+                {
+                    TestSuccessOrFailString = "TEST FAILED";
+                }
+
+                //set the text box to show the result text in to visible
+                TestFieldVisibility = Visibility.Visible;
+
+
+            }
+            else
+            {
+                //TODO launch an error view complaining about the database fields
+            }
+
+
         }
 
         #endregion
@@ -59,6 +133,31 @@ namespace WPFUI.ViewModels
             {
                 //TODO write Oracle settings
             }
+        }
+
+        private bool CheckDBFields()
+        {
+            if (DBFieldData[0] is "WW")
+            {
+                return false;
+            }
+            else if (DBFieldData[1] is "XX")
+            {
+                return false;
+            }
+            else if (DBFieldData[2] is "YY")
+            {
+                return false;
+            }
+            else if (DBFieldData[3] is "ZZ")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
         }
 
         #endregion
