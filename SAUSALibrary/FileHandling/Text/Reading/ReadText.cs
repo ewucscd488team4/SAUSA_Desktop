7dce26db3580b7fb96f79f160e33b3c3adb87941
Sarah Marchinko <smarchinko@eagles.ewu.edu>
2020-02-19 12:51:38 -0800

    Update ReadText.cs

--- SAUSALibrary/FileHandling/Text/Reading/ReadText.cs
+++ SAUSALibrary/FileHandling/Text/Reading/ReadText.cs
@@ -11,6 +11,8 @@ namespace SAUSALibrary.FileHandling.Text.Reading
 {
     public class ReadText
     {
+        private const string UNITY_NEWCONTAINER = "EmbedTest\\ToGUI.csv";
+
         public static bool ReadUnityCSV(string fqFilePath)
         {
             return false;
@@ -25,7 +27,7 @@ namespace SAUSALibrary.FileHandling.Text.Reading
         /// <param name="modelList"></param>
         /// <returns></returns>
         /// 
-        public static ObservableCollection<FullStackModel> ConvertCSVToStack (string fullCSVFilePath, string CSVFileName)
+        public static ObservableCollection<FullStackModel> ConvertCSVToStack(string fullCSVFilePath, string CSVFileName)
         {
             ObservableCollection<FullStackModel> modelList = new ObservableCollection<FullStackModel>();
             var fqCSVFileName = Path.Combine(fullCSVFilePath, CSVFileName);
@@ -40,6 +42,57 @@ namespace SAUSALibrary.FileHandling.Text.Reading
                 modelList.Add(new FullStackModel(long.Parse(FSMline[0]), long.Parse(FSMline[1]), long.Parse(FSMline[2]), long.Parse(FSMline[3]), long.Parse(FSMline[4]), long.Parse(FSMline[5]), long.Parse(FSMline[6]), long.Parse(FSMline[7]), FSMline[8]));
             }
 
+            return modelList;
+        }
+
+        /// <summary>
+        /// Read from a CSV the ID and position values into a FullStackModel.
+        /// 
+        /// </summary>
+        /// <param name="fullCSVFilePath"></param>
+        /// <param name="CSVFileName"></param>
+        /// <param name="modelList"></param>
+        /// <returns></returns>
+        /// 
+        public static ObservableCollection<FullStackModel> ConvertCSVToPositionStack (string fullCSVFilePath, string CSVFileName)
+        {
+            ObservableCollection<FullStackModel> modelList = new ObservableCollection<FullStackModel>();
+            var fqCSVFileName = Path.Combine(fullCSVFilePath, CSVFileName);
+
+            //StreamReader stream = new StreamReader(fqCSVFileName);
+            //var lines = File.ReadLines(AppDomain.CurrentDomain.BaseDirectory.ToString() + "\\EmbedTest\\ToGUI.csv");
+            var lines = File.ReadLines(fqCSVFileName);
+
+            foreach (string container in lines)
+            {
+                string[] FSMline = container.Split(',');
+                modelList.Add(new FullStackModel(long.Parse(FSMline[0]), long.Parse(FSMline[1]), long.Parse(FSMline[2]), long.Parse(FSMline[3]), 0,0,0,0,null));
+            }
+
+            return modelList;
+        }
+
+        ///<summary>
+        ///Compare a Position Stack and a Full Stack and update the Full Stack
+        /// </summary>
+        ///<param name="PositionStack">FullStack with just ID and X,Y,Z position</param>
+        ///<param name="ContainerStack">FullStack with all Containers listed currently</param>
+        ///<returns>Observable Collection of FullStack</returns>
+        ///
+
+        public static ObservableCollection<FullStackModel> ComparePositionStackToCurrentStack (ObservableCollection<FullStackModel> PositionStack, ObservableCollection<FullStackModel> StackToUpdate)
+        {
+            ObservableCollection<FullStackModel> modelList = new ObservableCollection<FullStackModel>();
+            foreach(var model in PositionStack)
+            {
+                foreach(var modelToUpdate in StackToUpdate)
+                {
+                    if (modelToUpdate.Index.Equals(model.Index))
+                    {
+                        modelList.Add(new FullStackModel(model.Index, model.XPOS, model.YPOS, model.ZPOS, modelToUpdate.Length, modelToUpdate.Width, modelToUpdate.Height, modelToUpdate.Weight, modelToUpdate.CrateName));
+                    }
+                }
+            }
             return modelList;
         }
     }
